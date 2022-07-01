1cfb482e170d4b9090ec0366eb4d159ce67c67b2
songulates <songl.ates.02@gmail.com>
2022-07-01 15:33:58 +0300

    if else

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace console_programlama
+{ 
+    class Program
+    {
+        static void Main(string[] args)//
+        {
+            int time=DateTime.Now.Hour;
+            if(time>=6 && time<11)
+            
+                Console.WriteLine("gündüz");
+            
+            
+            else if(time<=18)
+            
+                Console.WriteLine("iyi günler");
+            else
+            
+                Console.WriteLine("gece");
+            string sonuc=time<=18 ? "iyi günler": "iyi geceler";
+            sonuc=time>=6 && time<11 ?  "günaydın":time<=18 ? "iyi günler":"gece";
+            Console.WriteLine(sonuc);
+
+            
+           
+        }
+    }
+}
+
